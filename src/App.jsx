2c171b12f2e21ebbcfe5bfb574ec85c14ee1bb8d
Adnan <87126125+Adnan42@users.noreply.github.com>
2024-05-06 16:01:06 +0600

    chore: Refactor App component and improve cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ const App = () => {
   const [size, setSize] = useState();
   const [open, setOpen] = useState(false);
   const showDrawer = () => {
-    setSize('large');
+    setSize("large");
     setOpen(!open);
   };
- 
+
   return (
     <>
       <div className="my-container d-flex align-items-center justify-content-between p-2  container-border">
@@ -59,25 +59,90 @@ const App = () => {
           </div>
           <div className="d-flex justify-content-center align-content-center  px-3">
             <CiSearch size={50} className=" mx-3 rounded-2  p-2 icon-border" />
-           <div onClick={showDrawer} className="cart-number">
-             <sup className="p-2 rounded-circle border ">8</sup>
-           <MdOutlineShoppingCart
-              size={50}
-              className=" rounded-2  p-2 icon-border"
-            />
-             <Drawer  open={open}  className="ant-header"  placement="right"size={size}>
-              <div>
-              <span className="icon-size" onClick={onClose}>
-              <IoIosArrowBack />
-              </span>
-              <span>Cart Details</span>
+            <div onClick={showDrawer} className="cart-number">
+              <sup className="p-2 rounded-circle border ">8</sup>
+              <MdOutlineShoppingCart
+                size={50}
+                className=" rounded-2  p-2 icon-border"
+              />
+              <Drawer
+                open={open}
+                className="ant-header"
+                placement="right"
+                size={size}
+              >
+                <div>
+                  <div className="d-flex justify-content-between align-content-center ">
+                    <div>
+                      <span className="icon-size" onClick={onClose}>
+                        <IoIosArrowBack />
+                      </span>
+                      <span>Cart Details</span>
+                    </div>
+                    <Button type="primary" className="mx-3">
+                      ODC
+                    </Button>
+                  </div>
 
-              </div>
-           
-           
-            </Drawer>
-          
-           </div>
+                  <div className="d-flex justify-content-between ">
+                  <div className="d-flex ">
+                    <span className="setmr">1.</span>
+                    <div style={{width:"85%"}}>
+                    <div className="d-flex gap-1">
+              <p>1</p>
+              <span>
+                <IoCloseCircle />
+              </span>
+              <p>Martsk | Container - 20' GP Standard</p>
+            </div>
+                      <div style={{width:"150%"}} className=" border rounded-5  border-dark py-2 px-4  ">
+                        <div className="">
+                          <div className="">
+                            <div className="">
+                              <div
+                                style={{ fontSize: "14px" }}
+                                className="d-flex justify-content-between align-items-center mb-2"
+                              >
+                                <div>Nhava Sheva</div>
+                                <div>19 days</div>
+                                <div>Shanghai</div>
+                              </div>
+                              <div className="d-flex justify-content-between align-items-center mb-4">
+                                <div className="step-circle"></div>
+                                <div className="line"></div>
+                                <div className="step-circle"></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    </div>
+                    <div className=" ">
+                      <div
+                        className="d-flex flex-column  justify-content-center align-items-center  "
+                        style={{ height: "100%" }}
+                      >
+                        <h6 style={{ fontSize: "12px" }} className="">
+                        Container - 20' GP Standard
+                        </h6>
+                        <div className="d-flex flex-column  justify-content-center align-items-center  ">
+                          <h6 style={{ fontSize: "12px",margin: "20px 0" }} className="">
+                            100$
+                          </h6>
+                         <div className="border d-flex ">
+                          <span className="border py-1 px-3 ">+</span>
+                          <span className="border py-1 px-3 ">1</span>
+                          <span className="border py-1 px-3 ">- </span>
+                         </div>
+                        </div>
+                       
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Drawer>
+            </div>
           </div>
         </div>
       </div>
